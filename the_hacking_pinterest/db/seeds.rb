4351e3bd4c41682f2c46b3e9dfbde91ed78859b5
MargVander <margauxvanderbeken@gmail.com>
2019-01-30 20:37:46 +0100

    modif du seed

--- the_hacking_pinterest/db/seeds.rb
+++ the_hacking_pinterest/db/seeds.rb
@@ -1,12 +1,12 @@
 require 'faker'
 
-# 20.times do
-# 	user = User.create!(email: Faker::Internet.email, username: Faker::FunnyName.name)
-# end
+20.times do
+	user = User.create!(email: Faker::Internet.email, username: Faker::FunnyName.name)
+end
 
-# 40.times do
-# 	pin = Pin.create!(user_id: rand(1..20), pic: Faker::Internet.url)
-# end
+40.times do
+	pin = Pin.create!(user_id: rand(1..20), pic: Faker::Internet.url)
+end
 
 50.times do
 	com = Comment.create!(user_id: rand(1..20), pin_id: rand(1..10), content: Faker::MichaelScott.quote)
